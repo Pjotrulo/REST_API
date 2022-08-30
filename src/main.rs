868c8ae0be8465e655c2ef98eb0f8bc6d4ee2003
Pjotrulo <piotrgdwn@gmail.com>
2022-08-30 17:12:19 +0200

    Added files

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+// https://rocket.rs/v0.5-rc/guide/requests/
+
+use rocket::*;
+use rand::Rng;
+
+fn main() {
+
+    #[get("/calculateDisselUsageForDistance/<distance>/<_year_of_production>/<fuel_usage_per100_km>")]
+    fn calc_fuel_usage(distance: u64, _year_of_production: u64, fuel_usage_per100_km: u64) -> String {
+        let fuel_usage = fuel_usage_per100_km * distance / 100;
+        format!{"FuelUsage is {}", fuel_usage}
+    }
+
+    type F = fraction::Fraction;
+
+    #[get("/probabilityOfUnitInjectorFail/<_vin>")]
+    fn calc_fail_prob(_vin: String) -> String {
+        let random_number = rand::thread_rng().gen_range(0..101);
+        let fail_probability = F::from(random_number) / F::from(100);
+        format!{"There is {:.2} fail probability", fail_probability}
+    }
+
+    rocket::ignite()
+        .mount(
+            "/",
+            routes![calc_fuel_usage, calc_fail_prob],
+        )
+        .launch();
+}
